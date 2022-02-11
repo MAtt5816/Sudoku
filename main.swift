53df49460db412d092c3c956df711ca665349ead
MAtt58 <>
2022-02-11 10:53:38 +0000

    Genering random numbers in sudoku grid (without validation)

--- main.swift
+++ main.swift
@@ -4,12 +4,23 @@ class Sudoku
 
   init()
   {
-    self.grid = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+    generate()
   }
 
   func show()
   {
     print(grid)
+  }
+
+  func generate()
+  {
+    for row in grid.indices
+    {
+      for col in grid[row].indices
+      {
+        grid[row][col] = Int.random(in: 1...9)
+      }
+    }
   }
 }
 
