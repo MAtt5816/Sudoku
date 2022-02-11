6858cf1dd66dfacf6bf62ea5c143a0e031a4b74d
MAtt58 <>
2022-02-11 10:44:38 +0000

    Basis of Sudoku class

--- main.swift
+++ main.swift
@@ -1,1 +1,17 @@
-print("Hello World!")
+class Sudoku
+{
+  private var grid = [[Int]]()
+
+  init()
+  {
+    self.grid = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+  }
+
+  func show()
+  {
+    print(grid)
+  }
+}
+
+var sudoku = Sudoku()
+sudoku.show()
