fba7887fe8717a9bff766d6ff035af84cdee8f1e
Catamoutain Dev <m.szewczyk58@gmail.com>
2022-02-14 13:26:05 +0000

    Subsquare check improved

--- main.swift
+++ main.swift
@@ -25,10 +25,12 @@ class Sudoku
           if(validate(row, col, number))
           {
             grid[row][col] = number
+            print(number) //tmp
             isOK = true
           }
         }
       }
+      print(grid[row]) //tmp
     }
   }
 
@@ -44,10 +46,10 @@ class Sudoku
       {
         return false
       }
-    }
-    if(!valid_sq(row, col, number))
-    {
-      return false
+      if(!valid_sq(row, col, number))
+      {
+        return false
+      }
     }
     return true
   }
@@ -83,7 +85,38 @@ class Sudoku
 
   func valid_sq(_ row: Int, _ col: Int, _ number: Int) -> Bool
   {
-    return true
+    /*
+    Subsquares:
+    1. 2. 3.
+    4. 5. 6.
+    7. 8. 9.
+    */
+    var big_row: Int = row/3
+    var big_col: Int = col/3
+    if row%3 != 0 || row == 0
+    {
+      big_row += 1
+    }
+    if col%3 != 0 || col == 0
+    {
+      big_col += 1
+    }
+
+    var tmp_set = Set<Int>()
+    for r in (big_row*3-3)...(big_row*3-1)
+    {
+      for c in (big_col*3-3)...(big_col*3-1)
+      {
+        tmp_set.insert(grid[r][c])
+      }
+    }
+    if(tmp_set.insert(number).inserted){
+      return true
+    }
+    else
+    {
+      return false
+    }
   }  
 }
 
